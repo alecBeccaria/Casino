633d0e139181a21ba562d05a2551f5945efe225c
nathanielmalmrose <79717751+nathanielmalmrose@users.noreply.github.com>
2022-02-10 19:34:49 -0700

    Card and deck class

---
+++ Casino/Models/Card.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino.Models
+{
+    class Card
+    {
+        private int value { get; set; }
+        private string name { get; set; }
+        private Suit suit { get; set; }
+        private Color color { get; set; }
+        public enum Suit { Hearts, Spades, Diamonds, Clubs }
+        public enum Color { Black, Red }
+
+        public Card()
+        {
+            
+        }
+
+        public Card(int value, string name, Suit suit, Color color)
+        {
+            this.value = value;
+            this.name = name;
+            this.suit = suit;
+            this.color = color;
+        }
+
+
+    }
+}
