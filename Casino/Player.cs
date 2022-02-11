c6964b384fa68344f46aa39021367d7a7033e6ec
docsteampunk <larmore.daniel@gmail.com>
2022-02-10 19:02:47 -0700

    Adding Basic Player Class

---
+++ Casino/Player.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino
+{
+    class Player
+    {
+        private int cash { get; set; }
+        private int chips { get; set; }
+
+        public Player()
+        {
+            cash = 1000;
+            cash = 1000;
+        }
+    }
+}
