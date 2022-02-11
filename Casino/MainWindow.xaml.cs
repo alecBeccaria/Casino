48aea595bb035d5ef69c43261b11cd63b23d4347
alecBeccaria <80131757+alecBeccaria@users.noreply.github.com>
2022-02-10 19:37:49 -0700

    Main menu progress

--- Casino/MainWindow.xaml.cs
+++ Casino/MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Casino
 {
@@ -23,6 +10,21 @@ namespace Casino
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Play(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Settings(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Exit(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
