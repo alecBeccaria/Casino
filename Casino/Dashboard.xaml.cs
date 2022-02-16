16b39d724a89eaf11f38f12ba7c48b193f15fa2d
docsteampunk <larmore.daniel@gmail.com>
2022-02-15 19:34:32 -0700

    Changed stuff, not fully working? I think it's because of the fact that it's pulling from a player that resets?

--- Casino/Dashboard.xaml.cs
+++ Casino/Dashboard.xaml.cs
@@ -19,9 +19,62 @@ namespace Casino
     /// </summary>
     public partial class Dashboard : Window
     {
+        Player player = new Player();
+
         public Dashboard()
         {
             InitializeComponent();
+            LblChips.Text = "Casino Chips: " + player.chips;
+            LblBank.Text = "Bank Chips: " + player.cash;
+        }
+
+        private void OnDeposit(object sender, RoutedEventArgs e)
+        {
+            int numChips = player.chips;
+            int numCash = player.cash;
+
+            int value;
+            bool success = int.TryParse(txtChips.Text, out value);
+            Console.WriteLine(success);
+            if (success)
+            {
+                value = int.Parse(txtChips.Text);
+                numChips = numChips - value; ;
+                numCash = numCash + value;
+
+                player.cash = numCash;
+                player.chips = numChips;
+            }
+            else
+            {
+                MessageBox.Show("Please enter number only!");
+            }
+        }
+
+        private void OnWithdraw(object sender, RoutedEventArgs e)
+        {
+            int numChips = player.chips;
+            int numCash = player.cash;
+
+            int value;
+            bool success = int.TryParse(txtChips.Text, out value);
+            Console.WriteLine(success);
+            if (success)
+            {
+                value = int.Parse(txtChips.Text);
+                numChips = numChips + value;
+                numCash = numCash - value;
+
+                player.cash = numCash;
+                player.chips = numChips;
+
+                LblChips.Text = "Casino Chips: " + numCash;
+                LblBank.Text = "Bank Chips: " + numChips;
+            }
+            else
+            {
+                MessageBox.Show("Please enter number only!");
+            }
         }
     }
 }
