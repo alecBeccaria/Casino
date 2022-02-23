c7cab4b61f4f3fcc8af77919fbb829244aa940ed
alecBeccaria <80131757+alecBeccaria@users.noreply.github.com>
2022-02-22 21:36:56 -0700

    Fixing Branch
    Please Work

---
+++ Navigation/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+
+namespace Navigation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void btnClickP1(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Page1();
+        }
+
+        private void btnClickP2(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Page2();
+        }
+    }
+}
