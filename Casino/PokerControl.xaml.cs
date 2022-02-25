f43b0eb491562f30ae35a293c7deac912b7a51d2
alecBeccaria <80131757+alecBeccaria@users.noreply.github.com>
2022-02-24 17:35:52 -0700

    Created a hold card system

--- Casino/PokerControl.xaml.cs
+++ Casino/PokerControl.xaml.cs
@@ -23,45 +23,183 @@ namespace Casino
     public partial class PokerControl : UserControl
     {
         Poker pokerGame;
+
+
         public PokerControl()
         {
             InitializeComponent();
             pokerGame = new Poker();
             pokerGame.initialize();
             List<Card> deck = pokerGame.deck.getDeck();
-            card1.Source = deck[0].image.Source;
-            card2.Source = deck[1].image.Source;
-            card3.Source = deck[2].image.Source;
-            card4.Source = deck[3].image.Source;
-            card5.Source = deck[4].image.Source;
-            txtBalance.Content = "Balance: Add bank connection";
-            
+            updateBalance(0);
+            for(int i = 0; i < 5; i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        card1.Source = deck[i].image.Source;
+                        break;
+                    case 1:
+                        card2.Source = deck[i].image.Source;
+                        break;
+                    case 2:
+                        card3.Source = deck[i].image.Source;
+                        break;
+                    case 3:
+                        card4.Source = deck[i].image.Source;
+                        break;
+                    case 4:
+                        card5.Source = deck[i].image.Source;
+                        break;
 
+                }  
+            }
+        }
+
+        private void updateBalance(int value)
+        {
+            Player.chips -= value;
+            txtBalance.Content = $"Balance: {Player.chips}";
         }
 
         private void btnHold1_Click(object sender, RoutedEventArgs e)
         {
-
+            if (btnHold1.Content.Equals("Hold"))
+            {
+                pokerGame.heldCards[0] = true;
+                btnHold1.Content = "un-hold";
+            }
+            else
+            {
+                pokerGame.heldCards[0] = false;
+                btnHold1.Content = "Hold";
+            }
         }
 
         private void btnHold2_Click(object sender, RoutedEventArgs e)
         {
-
+            if (btnHold2.Content.Equals("Hold"))
+            {
+                pokerGame.heldCards[1] = true;
+                btnHold2.Content = "un-hold";
+            }
+            else
+            {
+                pokerGame.heldCards[1] = false;
+                btnHold2.Content = "Hold";
+            }
         }
 
         private void btnHold3_Click(object sender, RoutedEventArgs e)
         {
-
+            if (btnHold3.Content.Equals("Hold"))
+            {
+                pokerGame.heldCards[2] = true;
+                btnHold3.Content = "un-hold";
+            }
+            else
+            {
+                pokerGame.heldCards[2] = false;
+                btnHold3.Content = "Hold";
+            }
         }
 
         private void btnHold4_Click(object sender, RoutedEventArgs e)
         {
-
+            if (btnHold4.Content.Equals("Hold"))
+            {
+                pokerGame.heldCards[3] = true;
+                btnHold4.Content = "un-hold";
+            }
+            else
+            {
+                pokerGame.heldCards[3] = false;
+                btnHold4.Content = "Hold";
+            }
         }
 
         private void btnHold5_Click(object sender, RoutedEventArgs e)
         {
+            if (btnHold5.Content.Equals("Hold"))
+            {
+                pokerGame.heldCards[4] = true;
+                btnHold5.Content = "un-hold";
+            }
+            else
+            {
+                pokerGame.heldCards[4] = false;
+                btnHold5.Content = "Hold";
+            }
+        }
 
+        private void chip1Click(object sender, MouseButtonEventArgs e)
+        {
+            pokerGame.potValues["1"]++;
+            updateBalance(1);
+            txtChip1.Content = pokerGame.potValues["1"].ToString();
+        }
+
+        private void chip5Click(object sender, MouseButtonEventArgs e)
+        {
+
+            pokerGame.potValues["5"]++;
+            updateBalance(5);
+            txtChip5.Content = pokerGame.potValues["5"].ToString();
+
+        }
+
+        private void chip10Click(object sender, MouseButtonEventArgs e)
+        {
+            pokerGame.potValues["10"]++;
+            updateBalance(10);
+            txtChip10.Content = pokerGame.potValues["10"].ToString();
+        }
+
+        private void chip20Click(object sender, MouseButtonEventArgs e)
+        {
+            pokerGame.potValues["20"]++;
+            updateBalance(20);
+            txtChip20.Content = pokerGame.potValues["20"].ToString();
+        }
+
+        private void chip50Click(object sender, MouseButtonEventArgs e)
+        {
+            pokerGame.potValues["50"]++;
+            updateBalance(50);
+            txtChip50.Content = pokerGame.potValues["50"].ToString();
+        }
+
+        private void chip100Click(object sender, MouseButtonEventArgs e)
+        {
+            pokerGame.potValues["100"]++;
+            updateBalance(100);
+            txtChip100.Content = pokerGame.potValues["100"].ToString();
+        }
+
+        private void chip500Click(object sender, MouseButtonEventArgs e)
+        {
+            pokerGame.potValues["500"]++;
+            updateBalance(500);
+            txtChip500.Content = pokerGame.potValues["500"].ToString();
+        }
+
+        private void chip1KClick(object sender, MouseButtonEventArgs e)
+        {
+            pokerGame.potValues["1K"]++;
+            updateBalance(1000);
+            txtChip1K.Content = pokerGame.potValues["1K"].ToString();
+        }
+
+        private void chip5KClick(object sender, MouseButtonEventArgs e)
+        {
+            pokerGame.potValues["5K"]++;
+            updateBalance(5000);
+            txtChip5K.Content = pokerGame.potValues["5K"].ToString();
+        }
+
+        private void btnHold_Click(object sender, RoutedEventArgs e)
+        {
+             
         }
     }
 }
--- Casino/PokerGame/Poker.cs
+++ Casino/PokerGame/Poker.cs
@@ -14,12 +14,33 @@ namespace Casino.PokerGame
     {
         public Deck deck { get; set; }
 
-        
+        public Dictionary<string, int> potValues;
+
+        public bool[] heldCards = new bool[] { false, false, false, false, false };
+
+
 
         public string url { get; set; }
         public void initialize()
         {
             deck = new Deck();
+            potValues = new Dictionary<string, int>() 
+            {
+                {"1", 0},
+                {"5", 0},
+                {"10", 0},
+                {"20", 0},
+                {"50", 0},
+                {"100", 0},
+                {"500", 0},
+                {"1K", 0},
+                {"5K", 0}
+                
+            };
+            
+      
+
+
             List<Card> cardList = deck.getDeck();
             Trace.WriteLine(cardList[0].suit.ToString());
             Trace.WriteLine(cardList[0].value.ToString());
@@ -27,6 +48,8 @@ namespace Casino.PokerGame
 
             deck.shuffleDeck();
             
+
+
 
         }
 
